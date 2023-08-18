582f927b8d76ba0a52fd36ecc2a9f07cd0962d5a
Jesse <jesseguerrero1991@gmail.com>
2023-08-18 16:03:52 -0500

    Changed to relative paths

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import Carousel from '@/components/carousel'
+import Carousel from '../components/carousel'
 //https://mccdc.com/
 export default function Home() {
   return (
